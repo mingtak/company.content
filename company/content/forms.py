4c154f71a8f4f76957c8015e7c99c897da1055ac
andy fang <andy@mingtak.com.tw>
2015-03-27 17:15:20 +0000

    modify event content type

--- company/content/forms.py
+++ company/content/forms.py
@@ -1,6 +1,7 @@
 from plone.dexterity.browser.add import DefaultAddForm, DefaultAddView
 from plone.dexterity.browser.edit import DefaultEditForm, DefaultEditView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from z3c.form.interfaces import HIDDEN_MODE
 
 
 class AddForm(DefaultAddForm):
@@ -17,3 +18,33 @@ class EditForm(DefaultEditForm):
 
 class EditView(DefaultEditView):
     form = EditForm
+
+
+class EventAddForm(DefaultAddForm):
+    template = ViewPageTemplateFile('template/addForm.pt')
+
+    def updateWidgets(self):
+        super(EventAddForm, self).updateWidgets()
+        self.widgets['location'].mode = HIDDEN_MODE
+        self.widgets['attendees'].mode = HIDDEN_MODE
+        self.widgets['contact_email'].mode = HIDDEN_MODE
+        self.widgets['event_url'].mode = HIDDEN_MODE
+
+
+class EventAddView(DefaultAddView):
+    form = EventAddForm
+
+
+class EventEditForm(DefaultEditForm):
+    template = ViewPageTemplateFile('template/editForm.pt')
+
+    def updateWidgets(self):
+        super(EventEditForm, self).updateWidgets()
+        self.widgets['location'].mode = HIDDEN_MODE
+        self.widgets['attendees'].mode = HIDDEN_MODE
+        self.widgets['contact_email'].mode = HIDDEN_MODE
+        self.widgets['event_url'].mode = HIDDEN_MODE
+
+class EventEditView(DefaultEditView):
+    form = EventEditForm
+
